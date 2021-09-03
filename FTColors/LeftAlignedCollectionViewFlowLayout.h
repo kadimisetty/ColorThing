b1c9d0d6d5987a4c0159d387dcbea005ddb72833
Sri Kadimisetty <kadimisetty@users.noreply.github.com>
2021-09-03 19:03:31 +0530

    add LeftAlignedCollectionViewFlowLayout to customize layout alignment

---
+++ FTColors/LeftAlignedCollectionViewFlowLayout.h
@@ -0,0 +1,16 @@
+//
+//  LeftAlignedCollectionViewFlowLayout.h
+//  FTColors
+//
+//  Created by Sri Krishna Kadimisetty on 9/3/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LeftAlignedCollectionViewFlowLayout : NSCollectionViewFlowLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FTColors/LeftAlignedCollectionViewFlowLayout.m
@@ -0,0 +1,48 @@
+//
+//  LeftAlignedCollectionViewFlowLayout.m
+//  FTColors
+//
+//  Created by Sri Krishna Kadimisetty on 9/3/21.
+//
+
+#import "LeftAlignedCollectionViewFlowLayout.h"
+
+@implementation LeftAlignedCollectionViewFlowLayout
+
+- (NSArray<__kindof NSCollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(NSRect)rect {
+
+    // READ: https://stackoverflow.com/a/36285721/225903
+    // WARNING: Doesn't account for items of varying heights.
+    
+    NSArray<__kindof NSCollectionViewLayoutAttributes *>
+    *defaultAttributes =
+        [super layoutAttributesForElementsInRect:rect];
+
+    if (![defaultAttributes count]) {
+        return defaultAttributes;
+    } else {
+        NSMutableArray<__kindof NSCollectionViewLayoutAttributes *>
+        *leftAlignedAttributes = [[NSMutableArray alloc] init];
+
+        CGFloat xCursor = self.sectionInset.left;
+        CGFloat lastYPosition = defaultAttributes[0].frame.origin.y;
+
+        for (NSCollectionViewLayoutAttributes *attributes in defaultAttributes) {
+            if (attributes.frame.origin.y > lastYPosition) {
+                xCursor = self.sectionInset.left;
+                lastYPosition = attributes.frame.origin.y;
+            }
+
+            NSRect asFrame = attributes.frame;
+            asFrame.origin.x = xCursor;
+            [attributes setFrame:asFrame];
+
+            xCursor += attributes.frame.size.width + self.minimumInteritemSpacing;
+            [leftAlignedAttributes addObject:attributes];
+        }
+        
+        return leftAlignedAttributes;
+    }
+}
+
+@end
